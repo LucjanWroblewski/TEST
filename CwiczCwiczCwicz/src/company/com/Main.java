bab0254fa30c57717bee041d60d7473120a544d1
Lucjan Wroblewski <39348086+LucjanWroblewski@users.noreply.github.com>
2018-06-15 15:55:02 +0200

    Add files via upload
    Cwiczenia z samouczka

---
+++ CwiczCwiczCwicz/src/company/com/Main.java
@@ -0,0 +1,15 @@
+package company.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double x = Math.random();
+        System.out.println(x);
+        if( x < 0) {
+            System.out.println("Temperatura ponizej 0");}
+            else {
+            System.out.println("Temperatura powyzej 0");
+
+        }
+    }
+}
