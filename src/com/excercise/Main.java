621df460cf07d8c77e0a23e501e0da6931405f7b
lucjanwroblewski <lucjan.wroblewski@gmail.com>
2018-08-15 14:19:03 +0200

    lesson 2

--- src/com/excercise/Main.java
+++ src/com/excercise/Main.java
@@ -3,6 +3,14 @@ package com.excercise;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Party Begins!");
+        System.out.println("Helo!i heard that You just start to programing");
+        boolean b = true;
+        System.out.println(b);
+        char Y;
+        char X;
+        System.out.println("Do you like "+ 'y' + 'x' +'?');
+        System.out.println(b);
+
+
     }
 }
