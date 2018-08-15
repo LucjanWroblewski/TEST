67d900fd961fcfb010b2225bfc86a03d565d532e
lucjanwroblewski <lucjan.wroblewski@gmail.com>
2018-08-15 14:39:35 +0200

    lesson 3

--- src/com/excercise/Main.java
+++ src/com/excercise/Main.java
@@ -11,6 +11,11 @@ public class Main {
         System.out.println("Do you like "+ 'y' + 'x' +'?');
         System.out.println(b);
 
-
+        String marka = "Opel";
+        String model = "Corsa";
+        String rocznik = "2018";
+        String cena = "50k PLN";
+        System.out.println(marka + " " + model + " " + rocznik + " " + cena + " ");
     }
+
 }
